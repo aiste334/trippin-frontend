826c26f6292bac9c03b3c43be7cbe2f0d382faae
Glória <63670594+VingardiumGloriosa@users.noreply.github.com>
2023-09-18 15:49:42 +0200

    functional post

--- scripts/add.js
+++ scripts/add.js
@@ -6,9 +6,34 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = "/"
     })
     
-    saveButton.addEventListener('click', saveDestination)
+    saveButton.addEventListener('click', saveDestination);
 });
 
 function saveDestination() {
-    
+    console.log("FUCK")
+    const form = document.getElementById("destination-form");
+    const formData = new FormData(form);
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+        formDataObject[key] = value;
+    });
+
+    fetch("http://localhost:3009/destination", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formDataObject)
+    })
+    .then(response => {
+        if (response.status === 201) {
+            alert("Destination created successfully!");
+        } else {
+            alert("Failed to create destination.");
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert("An error occurred while creating the destination.");
+    });
 }
